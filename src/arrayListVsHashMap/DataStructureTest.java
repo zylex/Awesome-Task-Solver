447fa58e309cc67e08210672ea07bdb651192dda
Daniel Spitzer <dsp@tradeshift.com>
2011-05-11 16:46:50 +0200

    beautified strings in DataStructureTest

--- src/arrayListVsHashMap/DataStructureTest.java
+++ src/arrayListVsHashMap/DataStructureTest.java
@@ -16,36 +16,39 @@ public class DataStructureTest {
 			h.put(i, new Obj(i, "" + i));
 			al.add(new Obj(i, "" + i));
 		}
-		System.out.println("Iteration measurement:");
+		System.out.println("Iteration speed measurement:");
 
 		long startTime = System.nanoTime();
 		Iterator<Integer> it = h.keySet().iterator();
 		while (it.hasNext()) {
 			h.get(it.next());
 		}
-		System.out.println((System.nanoTime() - startTime) + " iterator");
-
+		System.out.println((System.nanoTime() - startTime) + " nanoseconds using HashMap+Iterator");
+		
+		long conversionTime = 0;
 		startTime = System.nanoTime();
 		ArrayList<Obj> a = new ArrayList<Obj>(h.values());
+		conversionTime = (System.nanoTime() - startTime);
 		int size = a.size();
 		for (int j = 0; j < size; j++) {
 			a.get(j);
 		}
-		System.out
-				.println((System.nanoTime() - startTime) + " alist converted");
+		System.out.println((System.nanoTime() - startTime) + " nanoseconds using HashMap->ArrayList conversion+for");
+		System.out.println("of which "+conversionTime + " nanoseconds is the HashMap->ArrayList conversion");
 
 		startTime = System.nanoTime();
 		int size2 = al.size();
 		for (int k = 0; k < size2; k++) {
 			al.get(k);
 		}
-		System.out.println((System.nanoTime() - startTime) + " alist 4realz");
+		System.out.println((System.nanoTime() - startTime) + " nanoseconds using ArrayList+for");
 
-		System.out.println("Get(50) measurement:");
+		System.out.println("");
+		System.out.println("Getting element #500 speed measurement:");
 
 		startTime = System.nanoTime();
 		h.get(500);
-		System.out.println((System.nanoTime() - startTime) + " hashmap");
+		System.out.println((System.nanoTime() - startTime) + " nanoseconds using HashMap's get() method");
 
 		startTime = System.nanoTime();
 		int size3 = al.size();
@@ -54,6 +57,7 @@ public class DataStructureTest {
 				break;
 			}
 		}
-		System.out.println((System.nanoTime() - startTime) + " alist 4realz");
+		System.out.println((System.nanoTime() - startTime) + " nanoseconds using ArrayList+for");
+		System.out.println("");
 	}
 }
